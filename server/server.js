c79e7441589d13e2f6bc6c8f7cf2f3f80bc89b50
aiaoio <snanilim@gmail.com>
2018-02-18 17:36:39 +0600

    added put delete

--- server/server.js
+++ server/server.js
@@ -95,6 +95,13 @@ app.use(function(req, res, next) {
 var userController = require('../app/Auth/userController');
 app.post('/signup', userController.signupPost);
 app.post('/login', userController.loginPost);
+
+app.put('/account', userController.ensureAuthenticated, userController.accountPut);
+app.delete('/account', userController.ensureAuthenticated, userController.accountDelete);
+app.post('/forgot', userController.forgotPost);
+app.post('/reset/:token', userController.resetPost);
+app.get('/unlink/:provider', userController.ensureAuthenticated, userController.unlink);
+
 app.post('/auth/facebook', userController.authFacebook);
 app.get('/auth/facebook/callback', userController.authFacebookCallback);
 app.post('/auth/google', userController.authGoogle);
